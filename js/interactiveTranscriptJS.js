e24d437b282a539bf9f04599c3d0905331fa29ee
davidluz <davidmelo8@hotmail.com>
2016-01-04 13:26:55 -0200

    v 1.0
    Segue

---
+++ js/interactiveTranscriptJS.js
@@ -0,0 +1,420 @@
+var videoPoints = [];
+var subtitle = [];
+var segundos = [];
+
+var legenda1 = 'Olá pessoal, essa é a nossa videoaula sobre saúde. por que discutir o conceito de saúde?';
+var legenda2 = 'Bom, porque nós estamos dentro de um projeto de educação que trata da saúde do trabalhador e da relação saúde e trabalho.';
+var legenda3 = "Pra discutir a relação saúde e trabalho a gente tem que começar conversando sobre o que é o conceito de saúde.";
+
+var legenda4 = "Porque predomina uma visão estritamente biólogica da saúde, muito centrada na visão médica da saúde, e a nossa ideia aqui é desconstruir essa visão, e problematizar o conceito de saúde, a partir da reflexão sobre as suas determinações sociais e especialmente as suas repercussões para os trabalhadores.";
+
+var legenda5 = "Nesse debate a nossa ideia é destacar como determinante o trabalho. como um determinante central do processo saúde e doença dos trabalhadores.";
+
+var legenda6 = "Bom, mas, falando sobre saúde...Se pedirmos às pessoas para que elas definam o que é saúde, são apresentadas dezenas de associações com várias dimensões da vida as condições ou modos de sobrevivência, com estados emocionais, com desejos, projetos de vida...";
+
+var legenda7 = "Pra discutir a relação saúde e trabalho a gente tem que começar conversando sobre o que é o conceito de saúde.";
+
+var legenda8 = "Pra discutir a relação saúde e trabalho a gente tem que começar conversando sobre o que é o conceito de saúde.";
+
+var legenda9 = "Pra discutir a relação saúde e trabalho a gente tem que começar conversando sobre o que é o conceito de saúde.";
+
+var legenda10 = "Pra discutir a relação saúde e trabalho a gente tem que começar conversando sobre o que é o conceito de saúde.";
+
+var legenda11 = "Pra discutir a relação saúde e trabalho a gente tem que começar conversando sobre o que é o conceito de saúde.";
+
+var legenda12 = "Pra discutir a relação saúde e trabalho a gente tem que começar conversando sobre o que é o conceito de saúde.";
+
+var legenda13 = "Pra discutir a relação saúde e trabalho a gente tem que começar conversando sobre o que é o conceito de saúde.";
+
+var legenda14 = "Pra discutir a relação saúde e trabalho a gente tem que começar conversando sobre o que é o conceito de saúde.";
+
+var legenda15 = "Pra discutir a relação saúde e trabalho a gente tem que começar conversando sobre o que é o conceito de saúde.";
+
+var legenda16 = "Pra discutir a relação saúde e trabalho a gente tem que começar conversando sobre o que é o conceito de saúde.";
+
+var legenda17 = "Pra discutir a relação saúde e trabalho a gente tem que começar conversando sobre o que é o conceito de saúde.";
+
+var legenda18 = "Pra discutir a relação saúde e trabalho a gente tem que começar conversando sobre o que é o conceito de saúde.";
+
+var legenda19 = "Pra discutir a relação saúde e trabalho a gente tem que começar conversando sobre o que é o conceito de saúde.";
+
+var legenda20 = "Pra discutir a relação saúde e trabalho a gente tem que começar conversando sobre o que é o conceito de saúde.";
+
+var legenda21 = "Pra discutir a relação saúde e trabalho a gente tem que começar conversando sobre o que é o conceito de saúde.";
+
+var legenda22 = "Pra discutir a relação saúde e trabalho a gente tem que começar conversando sobre o que é o conceito de saúde.";
+
+var legenda23 = "Pra discutir a relação saúde e trabalho a gente tem que começar conversando sobre o que é o conceito de saúde.";
+
+var legenda24 = "Pra discutir a relação saúde e trabalho a gente tem que começar conversando sobre o que é o conceito de saúde.";
+
+var legenda25 = "Pra discutir a relação saúde e trabalho a gente tem que começar conversando sobre o que é o conceito de saúde.";
+
+var legenda26 = "Pra discutir a relação saúde e trabalho a gente tem que começar conversando sobre o que é o conceito de saúde.";
+
+var legenda27 = "Pra discutir a relação saúde e trabalho a gente tem que começar conversando sobre o que é o conceito de saúde.";
+
+subtitle.push(legenda1);
+subtitle.push(legenda2);
+subtitle.push(legenda3);
+subtitle.push(legenda4);
+subtitle.push(legenda5);
+subtitle.push(legenda6);
+subtitle.push(legenda7);
+subtitle.push(legenda8);
+subtitle.push(legenda9);
+subtitle.push(legenda10);
+subtitle.push(legenda11);
+subtitle.push(legenda12);
+subtitle.push(legenda13);
+subtitle.push(legenda14);
+subtitle.push(legenda15);
+subtitle.push(legenda16);
+subtitle.push(legenda17);
+subtitle.push(legenda18);
+subtitle.push(legenda19);
+subtitle.push(legenda20);
+subtitle.push(legenda21);
+subtitle.push(legenda22);
+subtitle.push(legenda23);
+subtitle.push(legenda24);
+subtitle.push(legenda25);
+subtitle.push(legenda26);
+subtitle.push(legenda27);
+
+var ponto1 = 16;
+var ponto2 = 25;
+var ponto3 = 37;
+var ponto4 = 44;
+var ponto5 = 75;
+var ponto6 = 89;
+var ponto7 = 107;
+var ponto8 = 39;
+var ponto9 = 39;
+var ponto10 = 39;
+var ponto11 = 39;
+var ponto12 = 39;
+var ponto13 = 39;
+var ponto14 = 39;
+var ponto15 = 39;
+var ponto16 = 39;
+var ponto17 = 39;
+var ponto18 = 39;
+var ponto19 = 39;
+var ponto20 = 39;
+var ponto21 = 39;
+var ponto22 = 39;
+var ponto23 = 39;
+var ponto24 = 39;
+var ponto25 = 39;
+var ponto26 = 39;
+var ponto27 = 39;
+
+
+segundos.push(ponto1);
+segundos.push(ponto2);
+segundos.push(ponto3);
+segundos.push(ponto4);
+segundos.push(ponto5);
+segundos.push(ponto6);
+segundos.push(ponto7);
+segundos.push(ponto8);
+segundos.push(ponto9);
+segundos.push(ponto10);
+segundos.push(ponto11);
+segundos.push(ponto12);
+segundos.push(ponto13);
+segundos.push(ponto14);
+segundos.push(ponto15);
+segundos.push(ponto16);
+segundos.push(ponto17);
+segundos.push(ponto18);
+segundos.push(ponto19);
+segundos.push(ponto20);
+segundos.push(ponto21);
+segundos.push(ponto22);
+segundos.push(ponto23);
+segundos.push(ponto24);
+segundos.push(ponto25);
+segundos.push(ponto26);
+segundos.push(ponto27);
+
+ 
+var numberOfPoints = 27; // Necessário definir ao iniciar o plugin
+
+//
+function geraPontosNoVideo(){
+for(var n=0; n<=numberOfPoints; n++){
+var pointID = "#v1p"+n;
+videoPoints.push(pointID);
+}
+}
+
+
+// Função responsável por escrever os parágrafos da legenda no HTML 
+function escreveLegendasNoHtml(){
+var pointID;
+
+for(var n=0; n<=videoPoints.length; n++){
+pointID = 'id="v1p'+n+'"';
+$("#container-legenda").append('<center><p class="v1-legendas" '+ pointID + ' onClick="goToPoint('+segundos[n]+')">'+subtitle[n] +'</p></center>');
+}
+
+}
+
+
+
+$(document).ready(function(e) {
+$('#video_container').find('video').get(0).play();	
+geraPontosNoVideo();
+escreveLegendasNoHtml();
+setInterval(atualizaLegenda,500);
+
+});
+
+
+
+
+function atualizaLegenda(){
+	
+var pontoAtualNaTimeLine = $('#video_container').find('video').get(0).currentTime;	
+var pontoAtualNaTimeLineInteiro = parseInt(pontoAtualNaTimeLine);	
+
+
+
+if(pontoAtualNaTimeLineInteiro >ponto1 && pontoAtualNaTimeLineInteiro < ponto2){
+	$('.v1-legendas').css('background', 'transparent');
+	$('.v1-legendas').css('color', 'black');
+	$('#v1p0').css("background-color","#232370");
+	$('#v1p0').css("color","white");
+	
+		
+}
+
+
+if(pontoAtualNaTimeLineInteiro>ponto2 && pontoAtualNaTimeLineInteiro < ponto3){
+	$('.v1-legendas').css('background', 'transparent');
+	$('.v1-legendas').css('color', 'black');
+	$('#v1p1').css("background-color","#232370");
+	$('#v1p1').css("color","white");
+	$('#v1p1').animate({ scrollTop: containerlegendaTopPosition }, "fast");
+	
+	/*$('#container-legenda').scrollTop($('#container-legenda').scrollTop() + $("#v1p1").position().top);*/
+	
+
+}
+
+
+if(pontoAtualNaTimeLineInteiro>ponto3 && pontoAtualNaTimeLineInteiro < ponto4){
+	$('.v1-legendas').css('background', 'transparent');
+	$('.v1-legendas').css('color', 'black');
+	$('#v1p2').css("background-color","#232370");
+	$('#v1p2').css("color","white");
+		
+}
+
+if(pontoAtualNaTimeLineInteiro>ponto4 && pontoAtualNaTimeLineInteiro < ponto5){
+	$('.v1-legendas').css('background', 'transparent');
+	$('.v1-legendas').css('color', 'black');
+	$('#v1p2').css("background-color","#232370");
+	$('#v1p2').css("color","white");
+}
+
+
+if(pontoAtualNaTimeLineInteiro>ponto5 && pontoAtualNaTimeLineInteiro < ponto6){
+	$('.v1-legendas').css('background', 'transparent');
+	$('.v1-legendas').css('color', 'black');
+	$('#v1p2').css("background-color","#232370");
+	$('#v1p2').css("color","white");
+}
+
+
+if(pontoAtualNaTimeLineInteiro>ponto6 && pontoAtualNaTimeLineInteiro < ponto7){
+	$('.v1-legendas').css('background', 'transparent');
+	$('.v1-legendas').css('color', 'black');
+	$('#v1p2').css("background-color","#232370");
+	$('#v1p2').css("color","white");
+}
+
+
+if(pontoAtualNaTimeLineInteiro>ponto7 && pontoAtualNaTimeLineInteiro < ponto8){
+	$('.v1-legendas').css('background', 'transparent');
+	$('.v1-legendas').css('color', 'black');
+	$('#v1p2').css("background-color","#232370");
+	$('#v1p2').css("color","white");
+}
+
+
+if(pontoAtualNaTimeLineInteiro>ponto8 && pontoAtualNaTimeLineInteiro < ponto9){
+	$('.v1-legendas').css('background', 'transparent');
+	$('.v1-legendas').css('color', 'black');
+	$('#v1p2').css("background-color","#232370");
+	$('#v1p2').css("color","white");
+	
+	
+	
+	
+}
+
+
+if(pontoAtualNaTimeLineInteiro>ponto9 && pontoAtualNaTimeLineInteiro < ponto10){
+	$('.v1-legendas').css('background', 'transparent');
+	$('.v1-legendas').css('color', 'black');
+	$('#v1p2').css("background-color","#232370");
+	$('#v1p2').css("color","white");
+}
+
+
+if(pontoAtualNaTimeLineInteiro>ponto10 && pontoAtualNaTimeLineInteiro < ponto11){
+	$('.v1-legendas').css('background', 'transparent');
+	$('.v1-legendas').css('color', 'black');
+	$('#v1p2').css("background-color","#232370");
+	$('#v1p2').css("color","white");
+}
+
+
+if(pontoAtualNaTimeLineInteiro>ponto11 && pontoAtualNaTimeLineInteiro < ponto12){
+	$('.v1-legendas').css('background', 'transparent');
+	$('.v1-legendas').css('color', 'black');
+	$('#v1p2').css("background-color","#232370");
+	$('#v1p2').css("color","white");
+}
+
+
+if(pontoAtualNaTimeLineInteiro>ponto12 && pontoAtualNaTimeLineInteiro < ponto13){
+	$('.v1-legendas').css('background', 'transparent');
+	$('.v1-legendas').css('color', 'black');
+	$('#v1p2').css("background-color","#232370");
+	$('#v1p2').css("color","white");
+}
+
+
+if(pontoAtualNaTimeLineInteiro>ponto13 && pontoAtualNaTimeLineInteiro < ponto14){
+	$('.v1-legendas').css('background', 'transparent');
+	$('.v1-legendas').css('color', 'black');
+	$('#v1p2').css("background-color","#232370");
+	$('#v1p2').css("color","white");
+}
+
+
+if(pontoAtualNaTimeLineInteiro>ponto14 && pontoAtualNaTimeLineInteiro < ponto15){
+	$('.v1-legendas').css('background', 'transparent');
+	$('.v1-legendas').css('color', 'black');
+	$('#v1p2').css("background-color","#232370");
+	$('#v1p2').css("color","white");
+}
+
+
+if(pontoAtualNaTimeLineInteiro>ponto15 && pontoAtualNaTimeLineInteiro < ponto16){
+	$('.v1-legendas').css('background', 'transparent');
+	$('.v1-legendas').css('color', 'black');
+	$('#v1p2').css("background-color","#232370");
+	$('#v1p2').css("color","white");
+}
+
+
+if(pontoAtualNaTimeLineInteiro>ponto16 && pontoAtualNaTimeLineInteiro < ponto17){
+	$('.v1-legendas').css('background', 'transparent');
+	$('.v1-legendas').css('color', 'black');
+	$('#v1p2').css("background-color","#232370");
+	$('#v1p2').css("color","white");
+}
+
+
+if(pontoAtualNaTimeLineInteiro>ponto17&& pontoAtualNaTimeLineInteiro < ponto18){
+	$('.v1-legendas').css('background', 'transparent');
+	$('.v1-legendas').css('color', 'black');
+	$('#v1p2').css("background-color","#232370");
+	$('#v1p2').css("color","white");
+}
+
+
+if(pontoAtualNaTimeLineInteiro>ponto18 && pontoAtualNaTimeLineInteiro < ponto19){
+	$('.v1-legendas').css('background', 'transparent');
+	$('.v1-legendas').css('color', 'black');
+	$('#v1p2').css("background-color","#232370");
+	$('#v1p2').css("color","white");
+}
+
+
+if(pontoAtualNaTimeLineInteiro>ponto19 && pontoAtualNaTimeLineInteiro < ponto20){
+	$('.v1-legendas').css('background', 'transparent');
+	$('.v1-legendas').css('color', 'black');
+	$('#v1p2').css("background-color","#232370");
+	$('#v1p2').css("color","white");
+}
+
+
+if(pontoAtualNaTimeLineInteiro>ponto20 && pontoAtualNaTimeLineInteiro < ponto21){
+	$('.v1-legendas').css('background', 'transparent');
+	$('.v1-legendas').css('color', 'black');
+	$('#v1p2').css("background-color","#232370");
+	$('#v1p2').css("color","white");
+}
+
+if(pontoAtualNaTimeLineInteiro>ponto21 && pontoAtualNaTimeLineInteiro < ponto22){
+	$('.v1-legendas').css('background', 'transparent');
+	$('.v1-legendas').css('color', 'black');
+	$('#v1p2').css("background-color","#232370");
+	$('#v1p2').css("color","white");
+}
+
+if(pontoAtualNaTimeLineInteiro>ponto22 && pontoAtualNaTimeLineInteiro < ponto23){
+	$('.v1-legendas').css('background', 'transparent');
+	$('.v1-legendas').css('color', 'black');
+	$('#v1p2').css("background-color","#232370");
+	$('#v1p2').css("color","white");
+}
+
+if(pontoAtualNaTimeLineInteiro>ponto23 && pontoAtualNaTimeLineInteiro < ponto24){
+	$('.v1-legendas').css('background', 'transparent');
+	$('.v1-legendas').css('color', 'black');
+	$('#v1p2').css("background-color","#232370");
+	$('#v1p2').css("color","white");
+}
+
+if(pontoAtualNaTimeLineInteiro>ponto24 && pontoAtualNaTimeLineInteiro < ponto25){
+	$('.v1-legendas').css('background', 'transparent');
+	$('.v1-legendas').css('color', 'black');
+	$('#v1p2').css("background-color","#232370");
+	$('#v1p2').css("color","white");
+}
+
+if(pontoAtualNaTimeLineInteiro>ponto25 && pontoAtualNaTimeLineInteiro < ponto26){
+	$('.v1-legendas').css('background', 'transparent');
+	$('.v1-legendas').css('color', 'black');
+	$('#v1p2').css("background-color","#232370");
+	$('#v1p2').css("color","white");
+}
+
+if(pontoAtualNaTimeLineInteiro>ponto26 && pontoAtualNaTimeLineInteiro < ponto27){
+	$('.v1-legendas').css('background', 'transparent');
+	$('.v1-legendas').css('color', 'black');
+	$('#v1p2').css("background-color","#232370");
+	$('#v1p2').css("color","white");
+}
+
+if(pontoAtualNaTimeLineInteiro>ponto27){
+	$('.v1-legendas').css('background', 'transparent');
+	$('.v1-legendas').css('color', 'black');
+	$('#v1p2').css("background-color","#232370");
+	$('#v1p2').css("color","white");
+}
+
+
+
+
+
+
+
+}
+
+
+
+function goToPoint(ponto){
+	var pontoEscolhido = ponto;
+
+	$('#video_container').find('video').get(0).currentTime = pontoEscolhido; 
+	
+}
+
